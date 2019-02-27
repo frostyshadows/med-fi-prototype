37e75624038d21662cd8f55751b9b33be3ade6a8
ellebot <echau2008@gmail.com>
2019-02-27 13:28:46 -0800

    filters skeleton page

---
+++ filters.js
@@ -0,0 +1,25 @@
+$(".year > .btn").click(function(){
+    $(".year > .btn").removeClass("active");
+    $(this).addClass("active");
+});
+
+$(".year2 > .btn").click(function(){
+    $(".year2 > .btn").removeClass("active");
+    $(this).addClass("active");
+});
+
+$(".gender > .btn").click(function(){
+    $(".gender > .btn").removeClass("active");
+    $(this).addClass("active");
+});
+
+$(".meeting > .btn").click(function(){
+    $(".meeting > .btn").removeClass("active");
+    $(this).addClass("active");
+});
+
+$(".time > .btn").click(function(){
+    $(".time > .btn").removeClass("active");
+    $(this).addClass("active");
+});
+
