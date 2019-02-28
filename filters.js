e994c029bab460e9e49ccf754714dec19175570f
ellebot <echau2008@gmail.com>
2019-02-28 01:37:03 -0800

    added placeholder profile table with filters as a non-functional pop-up

--- filters.js
+++ filters.js
@@ -1,25 +1,65 @@
-$(".year > .btn").click(function(){
-    $(".year > .btn").removeClass("active");
-    $(this).addClass("active");
-});
+var profiles = [
+  {
+      "ID": "1",
+      "Name": "Victoria",
+      "Field": "Computer Engineer",
+      "Year": "4",
+      "Location": "Vancouver",
+      "Gender": "Female",
+      "Meeting": "Online",
+      "Time": "Weekly"
+  },
+  {
+      "ID": "1",
+      "Name": "Victoria",
+      "Field": "Computer Engineer",
+      "Year": "4",
+      "Location": "Vancouver",
+      "Gender": "Female",
+      "Meeting": "Online",
+      "Time": "Weekly"
+  },
+  {
+      "ID": "1",
+      "Name": "Victoria",
+      "Field": "Computer Engineer",
+      "Year": "4",
+      "Location": "Vancouver",
+      "Gender": "Female",
+      "Meeting": "Online",
+      "Time": "Weekly"
+  }
+];
 
-$(".year2 > .btn").click(function(){
-    $(".year2 > .btn").removeClass("active");
-    $(this).addClass("active");
-});
+// profile table creation
+var table = document.createElement("table");
+var col = [];
+for (var i = 0; i < profiles.length; i++) {
+    for (var key in profiles[i]) {
+        if (col.indexOf(key) === -1) {
+            col.push(key);
+        }
+    }
+}
+for (var i = 0; i < profiles.length; i++) {
+    tr = table.insertRow(-1);
 
-$(".gender > .btn").click(function(){
-    $(".gender > .btn").removeClass("active");
-    $(this).addClass("active");
-});
+    for (var j = 0; j < col.length; j++) {
+        var tabCell = tr.insertCell(-1);
+        tabCell.innerHTML = profiles[i][col[j]];
+    }
+}
 
-$(".meeting > .btn").click(function(){
-    $(".meeting > .btn").removeClass("active");
-    $(this).addClass("active");
-});
+var divContainer = document.getElementById("profiles");
+divContainer.innerHTML = "";
+divContainer.appendChild(table);
 
-$(".time > .btn").click(function(){
-    $(".time > .btn").removeClass("active");
+// Button functions
+$(".btn").click(function(){
+  if($(this).hasClass("active")) {
+    $(this).removeClass("active");
+  } else {
     $(this).addClass("active");
+  }
 });
 
